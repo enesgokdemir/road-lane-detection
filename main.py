9a186109267ba438de6b27519f57ec4f0bc55419
Enes Gökdemir <108132038+enesgokdemir@users.noreply.github.com>
2022-11-15 21:03:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def roi(image, vertices):
+    mask = np.zeros_like(image)
+    mask_color = 255
+    cv2.fillPoly(mask, vertices, mask_color)
+    cropped_img = cv2.bitwise_and(image, mask)
+    return cropped_img
+
+
+def draw_lines(image, hough_lines):
+    for line in hough_lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(image, (x1, y1), (x2, y2), (100, 250, 0), 2)
+
+    return image
+
+
+def process(img):
+    height = img.shape[0]
+    width = img.shape[1]
+    roi_vertices = [
+        (0, 275),
+        (2*width/3, 2*height/3),
+        (width, 1000)
+    ]
+
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray_img = cv2.dilate(gray_img, kernel=np.ones((3, 3), np.uint8))
+
+    canny = cv2.Canny(gray_img, 180, 200)
+
+    roi_img = roi(canny, np.array([roi_vertices], np.int32))
+
+    lines = cv2.HoughLinesP(roi_img, 1, np.pi / 180, threshold=15, minLineLength=15, maxLineGap=15)
+    final_img = draw_lines(img, lines)
+
+    return final_img
+
+
+cap = cv2.VideoCapture("video.mp4")
+
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fourcc = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')
+saved_frame = cv2.VideoWriter("lane_detection.avi", fourcc, 30.0, (frame_width, frame_height))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    try:
+        frame = process(frame)
+
+        saved_frame.write(frame)
+        cv2.imshow("video", frame)
+
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    except Exception:
+        break
+
+cap.release()
+saved_frame.release()
+cv2.destroyAllWindows()
